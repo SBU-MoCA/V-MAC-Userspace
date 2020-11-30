20aacf38376c35abf12825f2aa601feff5dabaa5
Mo <mohammed.elbadry@stonybrook.edu>
2020-11-30 13:30:04 -0500

    License added

--- vmac-usrsp.c
+++ vmac-usrsp.c
@@ -1,3 +1,16 @@
+/*
+* Copyright (c) 2017 - 2020, Mohammed Elbadry
+*
+*
+* This file is part of V-MAC (Pub/Sub data-centric Multicast MAC layer)
+*
+* V-MAC is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 
+* 4.0 International License.
+* 
+* You should have received a copy of the license along with this
+* work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+* 
+*/
 #include "vmac-usrsp.h"
 #include "uthash.h"
 
